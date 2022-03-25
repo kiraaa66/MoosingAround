bf793d485d0dbfd09e7d379e98a48b1fc652a77f
kiraaa66 <kiratarasuk@hotmail.ca>
2022-03-24 20:58:04 -0400

    stupid seg fault

--- moose3Kira.cpp
+++ moose3Kira.cpp
@@ -15,8 +15,6 @@
 
 using namespace std; 
 
-using namespace std;
-
 #define players 2
 #define fields 3
 #define runs 1
@@ -29,49 +27,40 @@ using namespace std;
 //function definitions
 int expFun(int x);
 
-int fitFUN(int);
+int fitFUN(vector<int> moose1);
 
 int main(){
 
     int i, j, k; //for loop variables
 
-    vector<int> fieldTracker(fields);
-    vector<int> mooseConsumption(players);
-
     default_random_engine rand(seed);
     uniform_int_distribution<int> fieldSelection(0, fields - 1);
 
-    vector<vector<int>> moosePlays(players); // potentially use an array, perk of vector is it is user defined length
-    vector<vector<int>> fieldGrowth(fields);
-
-
-    for (i = 0; i < players; i++){
-        moosePlays[i].reserve(turns);
-    }
-
-    for (i = 0; i < fields; i++){
-        fieldGrowth[i].reserve(turns);
-    }
-
-//set up the for loop to go through each of the runs
-
-
     //set up calculating the random score each time
+    vector<vector<int>> moosePlays(gens);
+    vector<int> mooseVals(gens);
 
-
-
+    for (k = 0; k < runs; k++){
+        for (i = 0; i < gens; i++){
+            moosePlays[i].reserve(turns);
+        }
+        for (i = 0; i < gens; i++){
+            for (j = 0; j < turns; j++){
+                moosePlays[i][j] = fieldSelection(rand);
+            }
+            mooseVals[i] = fitFUN(moosePlays[i]);
+            cout << "Moose Val Gen: " << i << "Turn: " << j << " " << mooseVals[i] << endl; 
+        }
+    }
 
 }
-
-
-
 int expFun(int x){
 
     return (10*exp(x))/(1 + exp(x)); // returns the function since it cannot be declared on its own
 
 } 
 
-int fitFUN(int moose1[turns]){
+int fitFUN(vector<int> moose1){
 
     vector<int> moose2(turns); // moose2 picks fo this particular round
 
@@ -103,20 +92,29 @@ int fitFUN(int moose1[turns]){
                 }
                 
             }
-
-
         }
-        for (j = 0; j < fields; j++){
+        else{
+            for (j = 0; j < fields; j++){
+                if (j == moose1[i]){
+                    moose1Val += (expFun(fieldNum[j]) - expFun(0));
+                    fieldNum[j] -= 1;
+                }
+                else if(j == moose2[i]){
+                    moose2Val += (expFun(fieldNum[j]) - expFun(0));
+                    fieldNum[j] -= 1;
+                }
+                else{
+                    fieldNum[j] += 1;
+                }
+            }
+        }
         
+        for (j = 0; j < fields; j++){
             if (fieldNum[j] <= 0){ // no field can go below zero, so it resets the value if it does hit zero
                 fieldNum[j] = 0;
             }
         }
 
     }
-
-
-
-
-
+    return moose1Val;
 }
